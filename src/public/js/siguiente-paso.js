0bafd61fddc1d0474417f1efa3726f0d0423ce72
brayanranda <brayanranda@gmail.com>
2020-10-20 14:51:49 -0500

    Ajustes css y js para el form de registrar

--- src/public/js/siguiente-paso.js
+++ src/public/js/siguiente-paso.js
@@ -1,10 +1,34 @@
-function next() {
+let siguiente1 = document.querySelectorAll("#siguiente1")[0];
+let siguiente2 = document.querySelectorAll("#siguiente2")[0];
+let cProfileRegSteps = document.querySelectorAll(".c-profile__reg-steps")[0];
 
-    
-    
-    if (document.getElementById("next_reg").getElementsByClassName("c-profile__reg-steps")[0].style.left = 'left: 0') {
-        document.getElementById("next_reg").getElementsByClassName("c-profile__reg-steps")[0].style.left = 'left: -100%;';
-    } else {
-        
-    }
-}
+let atras1 = document.querySelectorAll("#atras1")[0];
+let atras2 = document.querySelectorAll("#atras2")[0];
+
+siguiente1.addEventListener('click', function (e) {
+    e.preventDefault();
+    setTimeout(function(){
+		cProfileRegSteps.style.left = "-100%";
+	},0)
+});
+
+siguiente2.addEventListener('click', function (e) {
+    e.preventDefault();
+    setTimeout(function(){
+		cProfileRegSteps.style.left = "-200%";
+	},0)
+});
+
+atras1.addEventListener('click', function (e) {
+    e.preventDefault();
+    setTimeout(function(){
+		cProfileRegSteps.style.left = "0";
+	},0)
+});
+
+atras2.addEventListener('click', function (e) {
+    e.preventDefault();
+    setTimeout(function(){
+		cProfileRegSteps.style.left = "-100%";
+	},0)
+});
