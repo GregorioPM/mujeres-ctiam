7ed366ae07f6cacf938a70a5e5291b8715dda8bb
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2020-10-25 00:35:57 -0500

    se corrigió la forma de cargal el modal items del perfil

--- src/public/js/modal-perfil.js
+++ src/public/js/modal-perfil.js
@@ -1,26 +1,7 @@
-function modal_cuenta () {
-    var temp;
-    var res = document.getElementById("ventana-cuenta").innerHTML;
-    var newcode = document.getElementById("ventana-cuenta").innerHTML = 
-    "<div class=\"c-cuenta\">" + 
-        "<div class=\"c-cuenta__content\">" +
-            "<li class=\"c-cuenta__li\">" + 
-                "<a class=\"c-cuenta__li--a\" href=\"\"><i class=\"fas fa-user\"></i>Perfil</a>" +
-            "</li>" +
-            "<li class=\"c-cuenta__li\">" + 
-                "<a class=\"c-cuenta__li--a\" href=\"\"><i class=\"fas fa-star\"></i>Favoritos</a>" +
-            "</li>" +
-            "<li class=\"c-cuenta__li\">" +
-                "<a class=\"c-cuenta__li--a\" href=\"/login/logout\"><i class=\"fas fa-sign-out-alt\"></i>Cerrar sesión</a>" +
-            "</li>" +
-        "</div>" + 
-    "</div>";
-
-    if (res.length === 0) {
-        return newcode;
+function modal_profile() {
+    if (document.getElementById("ventana-cuenta").style.display == "none") {
+        document.getElementById("ventana-cuenta").style.display = "block";
     } else {
-        temp = document.getElementById("ventana-cuenta").innerHTML = "";
+        document.getElementById("ventana-cuenta").style.display = "none";
     }
-    return temp;
 }
-
