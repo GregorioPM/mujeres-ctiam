09a74d2c48adda88067a5aff77ac322e2bd9f1b1
Gregorio Perez <jose.gre1222@gmail.com>
2020-11-01 22:28:36 -0500

    gestionar categoria por admin

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,35 @@
+const { Category } = require("../repository/database").models;
+
+module.exports = {
+    getCategorys: async () => {
+        return await Category.findAll();
+    },
+    createACategory: async (req, res) => {
+        const { nombre, descripcion } = req.body;
+        const categorys = await Category.create({
+            nombre,
+            descripcion,
+        });
+        if (categorys) {
+            res.redirect("/admin/categorys");
+        }
+        res.redirect("/admin/categorys");
+    },
+    updateACategory: async (req, res) => {
+        const categoryDB = await Category.findByPk(req.params.id);
+        const category = req.body;
+        for (field in category) {
+            categoryDB[field] = category[field];
+        }
+        await categoryDB.save();
+        res.redirect("/admin/categorys");
+    },
+    deleteACategory: async (req, res) => {
+        const category = await Category.findByPk(req.params.id);
+        if (category) {
+            category.destroy();
+            return res.redirect("/admin/categorys");
+        }
+        return res.redirect("/admin/categorys");
+    },
+}
